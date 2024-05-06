5ddd00fc785f7a6766020051449fc2d54bfda7e1
unknown <kipkipinho@gmail.com>
2024-05-06 02:34:22 -0300

    style(file): Alteração do nome do arquivo de evento messageChange.ts para messageUpdate.ts

--- source/events/messageUpdate.ts
+++ source/events/messageUpdate.ts
@@ -1,7 +1,7 @@
 import { isBlacklisted, moderateMessage } from "../utils/blacklist";
 import { Events, Message } from "discord.js";
 
-export const eventName = Events.MessageCreate;
+export const eventName = Events.MessageUpdate;
 
 export const execute = async (message: Message) => {
 	if (isBlacklisted(message)) return await moderateMessage(message, "Envio de mensagem blacklisted.");
